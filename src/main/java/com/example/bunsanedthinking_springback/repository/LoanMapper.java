b29e0c0b653b02810ebd7f24ce9c1351fa36b50e
0711kc <0711kc@naver.com>
2024-10-31 21:49:36 +0900

    refactor: addLoanProduct Mapper 방식으로 변경

--- src/main/java/com/example/bunsanedthinking_springback/repository/LoanMapper.java
+++ src/main/java/com/example/bunsanedthinking_springback/repository/LoanMapper.java
@@ -1,7 +1,11 @@
 package com.example.bunsanedthinking_springback.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.bunsanedthinking_springback.vo.LoanVO;
 
 @Mapper
 public interface LoanMapper {
+	void insert_LoanManagement(@Param("loan") LoanVO loanVO);
 }
