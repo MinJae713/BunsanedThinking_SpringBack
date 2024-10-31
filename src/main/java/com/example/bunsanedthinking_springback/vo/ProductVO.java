3e9843749826d5928d05711a52fa4059d822163e
xP-xD <tlsqlfn842@naver.com>
2024-10-31 18:31:22 +0900

    feat: ProductVO, RecontractVO, ReportVO, RevivalVO, SalesVO, ServiceVO, SurgeryHistoryVO, TerminationVO VO추가

---
+++ src/main/java/com/example/bunsanedthinking_springback/vo/ProductVO.java
@@ -0,0 +1,10 @@
+package com.example.bunsanedthinking_springback.vo;
+
+import lombok.Data;
+
+@Data
+public class ProductVO {
+	private int id;
+	private String name;
+	private String maximum_money;
+}
