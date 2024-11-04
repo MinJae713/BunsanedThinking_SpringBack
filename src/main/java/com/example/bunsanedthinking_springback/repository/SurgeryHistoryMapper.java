b1a764765940e9e1071474b4214c8328b378f2d6
MinJae713 <dbalsend@naver.com>
2024-11-04 22:20:52 +0900

    [1104]1차 추합 완
    [1104]1차 추합 완

--- src/main/java/com/example/bunsanedthinking_springback/repository/SurgeryHistoryMapper.java
+++ src/main/java/com/example/bunsanedthinking_springback/repository/SurgeryHistoryMapper.java
@@ -1,21 +1,18 @@
 package com.example.bunsanedthinking_springback.repository;
 
-import com.example.bunsanedthinking_springback.vo.SurgeryHistoryVO;
 import com.example.bunsanedthinking_springback.entity.surgeryHistory.SurgeryHistory;
+import com.example.bunsanedthinking_springback.vo.SurgeryHistoryVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
-
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 @Mapper
 public interface SurgeryHistoryMapper {
     public List<SurgeryHistoryVO> getAllByCustomerId_Customer(int id);
 	ArrayList<SurgeryHistoryVO> get_UnderWritingModel(int id);
 	void insert_SalesModel(SurgeryHistoryVO surgeryHistoryVO);
-}
     public void insert_surgeryHistory_CustomerInformationManagement(@Param("surgeryHistory") SurgeryHistory surgeryHistory);
     public void deleteSurgeryHistoriesByCustomerId_CustomerInformationManagement(int customerId);
 
