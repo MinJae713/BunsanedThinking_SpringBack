bbe84f5ab7a83e0e6e070f1346dfdbaa9ae0f300
MinJae713 <dbalsend@naver.com>
2024-11-04 22:34:15 +0900

    [1104]추합완(CompDetail까지)
    [1104]추합완(CompDetail까지)

--- src/main/java/com/example/bunsanedthinking_springback/repository/PartnerCompanyMapper.java
+++ src/main/java/com/example/bunsanedthinking_springback/repository/PartnerCompanyMapper.java
@@ -1,20 +1,19 @@
 package com.example.bunsanedthinking_springback.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import com.example.bunsanedthinking_springback.entity.partnerCompany.PartnerCompany;
+import com.example.bunsanedthinking_springback.vo.PartnerCompanyVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import com.example.bunsanedthinking_springback.vo.PartnerCompanyVO;
+import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface PartnerCompanyMapper {
 	List<PartnerCompanyVO> findByPartnerType_CustomerSupport(int partnerType);
 
 	Optional<PartnerCompanyVO> findById_CustomerSupport(int id);
-  PartnerCompany findById_PartnerCompany(@Param("id") int id);
+  	PartnerCompany findById_PartnerCompany(@Param("id") int id);
 
 	void insert_CompensationPlanning(PartnerCompanyVO partnerCompanyVO);
 
