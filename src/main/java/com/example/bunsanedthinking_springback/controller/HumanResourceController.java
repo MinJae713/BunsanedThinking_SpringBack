f82eea62ea8c9aaaf7b2b763ef62dd92da45eb6c
0711kc <0711kc@naver.com>
2024-11-05 19:01:46 +0900

    refactor: HumanResourceModel URL에 employee 추가

--- src/main/java/com/example/bunsanedthinking_springback/controller/HumanResourceController.java
+++ src/main/java/com/example/bunsanedthinking_springback/controller/HumanResourceController.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/humanResource")
+@RequestMapping("/employee/humanResource")
 public class HumanResourceController {
 	@Autowired
 	private HumanResourceModel humanResourceModel;
