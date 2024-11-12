bcfae9ca759a8f78f0e1d1a88b2108813d8440f5
Administrator <tlsqlfn842@naver.com>
2024-11-12 14:04:31 +0900

    refactor : PaymentDetail~Termination @Data추가

--- src/main/java/com/example/bunsanedthinking_springback/entity/product/Product.java
+++ src/main/java/com/example/bunsanedthinking_springback/entity/product/Product.java
@@ -2,6 +2,8 @@ package com.example.bunsanedthinking_springback.entity.product;
 
 
 import com.example.bunsanedthinking_springback.vo.ProductVO;
+
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
  */
 
 @NoArgsConstructor
+@Data
 public abstract class Product implements Cloneable {
 
 	public static final int PRODUCT_SERIAL_NUMBER = 700;
@@ -24,29 +27,4 @@ public abstract class Product implements Cloneable {
 	}
 
 	public abstract Product clone();
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getMaximumMoney() {
-		return maximumMoney;
-	}
-
-	public void setMaximumMoney(int maximumMoney) {
-		this.maximumMoney = maximumMoney;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 }
