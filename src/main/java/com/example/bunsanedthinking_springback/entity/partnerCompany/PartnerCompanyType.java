9283cd173c1543451fd256be31c2a481cb189e54
MinJae713 <dbalsend@naver.com>
2024-11-14 13:30:59 +0900

    [FIX] 협력업체 enum 수정
    PartnerCompany 협력업체 타입이랑 Report에 협력업체 아이디의 불일치, PartnerCompany 협력업체 타입이랑 협력업체 Enum이 달라서 순서 및 종류 좀 수정했심다

--- src/main/java/com/example/bunsanedthinking_springback/entity/partnerCompany/PartnerCompanyType.java
+++ src/main/java/com/example/bunsanedthinking_springback/entity/partnerCompany/PartnerCompanyType.java
@@ -8,12 +8,16 @@ package com.example.bunsanedthinking_springback.entity.partnerCompany;
  */
 public enum PartnerCompanyType {
 	
-	Hospital("병원"),
-	RepairShop("카센터"),
-	LawFirm("법무법인"),
-	DamageAssessmentCompany("손해사정업체"),
-	RoadsideAssistanceCompany("긴급출동회사");
-	
+//	Hospital("병원"),
+//	RepairShop("카센터"),
+//	LawFirm("법무법인"),
+	// 협력업체의 레포트 받아오는 로직이
+	// 협력업체 더미의 타입이랑 안맞아서 일단 주석으로 막아놨슴다
+	// 협력업체 타입이 1. 긴급출동, 2. 손해사정이 되게끔 하다보니 0은 더미로 넣어놨어유
+	DumyCompany("더미"),
+	RoadsideAssistanceCompany("긴급출동회사"),
+	DamageAssessmentCompany("손해사정업체");
+
 	private String name;
 
 	PartnerCompanyType(String name) {
