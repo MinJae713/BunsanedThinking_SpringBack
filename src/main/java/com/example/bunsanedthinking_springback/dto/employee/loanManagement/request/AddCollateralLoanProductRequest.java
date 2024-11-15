207a595649725c28620981fd282f3008dda9a13a
0711kc <0711kc@naver.com>
2024-11-16 01:20:15 +0900

    refactor: LoanManagement DTO 추가 및 구조 변경, getAllLoanRequest 메서드 추가

--- src/main/java/com/example/bunsanedthinking_springback/dto/employee/loanManagement/request/AddCollateralLoanProductRequest.java
+++ src/main/java/com/example/bunsanedthinking_springback/dto/employee/loanManagement/request/AddCollateralLoanProductRequest.java
@@ -1,11 +1,11 @@
-package com.example.bunsanedthinking_springback.dto.employee.loanManagement;
+package com.example.bunsanedthinking_springback.dto.employee.loanManagement.request;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class AddCollateralLoanProductDTO {
+public class AddCollateralLoanProductRequest {
 	private int loanType;
 	private String name;
 	private int interestRate;
--- src/main/java/com/example/bunsanedthinking_springback/dto/employee/loanManagement/request/AddLoanProductRequest.java
+++ src/main/java/com/example/bunsanedthinking_springback/dto/employee/loanManagement/request/AddLoanProductRequest.java
@@ -1,11 +1,11 @@
-package com.example.bunsanedthinking_springback.dto.employee.loanManagement;
+package com.example.bunsanedthinking_springback.dto.employee.loanManagement.request;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class AddLoanProductDTO {
+public class AddLoanProductRequest {
 	private int loanType;
 	private String name;
 	private int interestRate;
